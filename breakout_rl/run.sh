2f0a8fa32cdeaab4f65491d8da73ba959c175ef6
Kirill Nagaitsev <knagaitsev@uchicago.edu>
2021-12-26 00:13:38 -0600

    add run script

---
+++ breakout_rl/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --mail-user=knagaitsev@uchicago.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/knagaitsev/slurm/out/%j.%N.stdout
+#SBATCH --error=/home/knagaitsev/slurm/out/%j.%N.stderr
+#SBATCH --partition=titan
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name=breakout_rl_train
+#SBATCH --mem-per-cpu=60000
+#SBATCH --time=4:00:00
+
+. /opt/conda/etc/profile.d/conda.sh
+conda activate /local/knagaitsev/nn
+python train.py
